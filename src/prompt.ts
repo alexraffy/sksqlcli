c1af43389bbdc82e837894b936db14dcc4299a75
alexraffy <92450474+alexraffy@users.noreply.github.com>
2022-05-19 22:32:16 +0200

    sksql update

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,6 +1,6 @@
 
 import * as readline from "readline";
-import {readTableAsJSON, SQLResult, SQLStatement} from "sksql";
+import {readTableAsJSON, readTableDefinition, serializeTableDefinition, SQLResult, SQLStatement} from "sksql";
 import {getState} from "./state";
 
 export function promptReady() {
@@ -15,38 +15,56 @@ export function promptReady() {
         let db = getState().db;
         let sql: SQLStatement;
         let ret: SQLResult;
-
-        try {
-            sql = new SQLStatement(db, input);
-            ret = sql.run() as SQLResult;
-            if (ret.error !== undefined) {
-                if (getState().output === "TEXT") {
-                    console.log(ret.error);
-                } else {
-                    console.log("{error: \"" + escape(ret.error) + "\"}");
-                }
-            } else if (ret.resultTableName !== "") {
-                let data = readTableAsJSON(db, ret.resultTableName);
-                if (getState().output === "TEXT") {
-                    console.table(data);
-                } else {
-                    let result = JSON.stringify(data);
-                    console.log(result);
+        if (input.startsWith("\\")) {
+            switch (input) {
+                case "\\ddl_tables":
+                {
+                    for (let i = 0; i < db.allTables.length; i++) {
+                        let tbl = db.allTables[i];
+                        let def = readTableDefinition(tbl.data, false);
+                        if (def.name.startsWith("#")) {
+                            continue;
+                        }
+                        if (["DUAL", "ROUTINES", "SYS_TABLE_STATISTICS"].includes(def.name.toUpperCase())) {
+                            continue;
+                        }
+                        console.log(serializeTableDefinition(db, def.name));
+                    }
                 }
             }
-        } catch (e) {
-            if (getState().silent === false) {
-                if (getState().output === "TEXT") {
-                    console.log(e.message);
-                } else {
-                    console.log("{error: \"" + escape(e.message) + "\"}");
+        } else {
+
+            try {
+                sql = new SQLStatement(db, input);
+                ret = sql.run() as SQLResult;
+                if (ret.error !== undefined) {
+                    if (getState().output === "TEXT") {
+                        console.log(ret.error);
+                    } else {
+                        console.log("{error: \"" + escape(ret.error) + "\"}");
+                    }
+                } else if (ret.resultTableName !== "") {
+                    let data = readTableAsJSON(db, ret.resultTableName);
+                    if (getState().output === "TEXT") {
+                        console.table(data);
+                    } else {
+                        let result = JSON.stringify(data);
+                        console.log(result);
+                    }
+                }
+            } catch (e) {
+                if (getState().silent === false) {
+                    if (getState().output === "TEXT") {
+                        console.log(e.message);
+                    } else {
+                        console.log("{error: \"" + escape(e.message) + "\"}");
+                    }
                 }
             }
+            if (sql !== undefined) {
+                sql.close();
+            }
         }
-        if (sql !== undefined) {
-            sql.close();
-        }
-
         if (getState().silent === false && ret !== undefined) {
             console.log(ret.totalRuntime.toFixed(2) + " ms");
         }
